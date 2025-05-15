b5818bd746ed1415f115f2b6e057942f503f1f44
temitope <zephtemi@gmail.com>
2025-05-15 18:39:07 +0100

    new  answer

---
+++ Aissgnment.py
@@ -0,0 +1,36 @@
+#lab work 1
+# n = int(input ("Enter a number Integer : "))
+# if n is odd m print  weird 
+# if n % 2 == 0:
+#     print("werid number")
+# if n is even in the range of 2 to 5 print not weird
+# elif 2 <= n <= 5:
+#         print("Not Weird")
+# if n is even in the range of 6 to 20 print weird
+# elif 6 <= n <= 20:
+#         print("Weird")
+# if n is even and greater than 20 print not weird
+# else:
+#         print("Not Weird")
+    
+
+#Lab work 2
+# we have a house price of 1000000
+house_price = 1000000
+
+#collect the user budget input 
+budget = int(input("Enter your budgetted amount: "))
+
+#To check if the budget and calculate down payment 
+if budget < 5000000 and budget > 300000:
+    downpayment = (10/100) * house_price
+    print("Your downpayment is 10%")
+elif budget  > 5000000:
+    downpayment = (20/100) * house_price
+    print("Your downpayment is 20%") 
+else:
+    print("Your budget does not qualify for a house")
+    
+
+#lab work 3 
+#aje i dont  kknow where to start from
